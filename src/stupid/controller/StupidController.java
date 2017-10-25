1ded8e71e2aba0ce41750c3820ee8ea07715eeea
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-25 12:17:31 -0600

    Made two arrays for words and numbers to practice how to use arrays.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -5,21 +5,49 @@ import java.util.Scanner;
 
 public class StupidController
 {
+	private String [] words;
+	private int [] numbers;
+	
 	public void start()
 	{
-		System.out.println("Ayyy Lmao");
+		//System.out.println("Ayyy Lmao");
 	
-		testScanner();
-	}
-
-	private void testScanner()
-	{
-		Scanner firstScanner;
-		firstScanner = new Scanner(System.in);
-		System.out.println("What is your favorite food?");
-		String answer = firstScanner.nextLine();
-		System.out.println("Oh cool, you like " + answer );
+		//testScanner();
 		
+		practiceArray();
 	}
 
+//	private void testScanner()
+//	{
+//		Scanner firstScanner;
+//		firstScanner = new Scanner(System.in);
+//		System.out.println("What is your favorite food?");
+//		String answer = firstScanner.nextLine();
+//		System.out.println("Oh cool, you like " + answer );
+//		
+//	}
+	
+	private void practiceArray()
+	{
+		System.out.println(words);
+		System.out.println(numbers);
+		
+		words = new String[4];
+		words[0] = "Hi, ";
+		words[1] = "How ";
+		words[2] = "are ";
+		words[3] = "you?";
+		for (String current : words)
+		{
+			System.out.println(current);
+		}
+		
+		numbers = new int[10];
+		for(int index = 0; index < 10; index ++)
+		{
+			numbers[index] = index;
+			
+			System.out.println(numbers[index]);
+		}
+	}
 }
